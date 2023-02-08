96b29ed61d0f65bff5dd8e8c5366922a5b5c3fd8
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-09 03:41:40 +0530

    welcome

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Length line = new Length(1, 2, 3, 4);
+            double length1 = line.CalculateLength();
+            Console.WriteLine("Length of 1st Line : " + length1);
+
+            Length line2 = new Length(5, 6, 7, 9);
+            double length2 = line2.CalculateLength();
+            Console.WriteLine("Length of 2nd Line : " + length2);
+
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("Both Line Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Both Line Are not Equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
