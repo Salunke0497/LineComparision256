dac93767a6ddd751880c8251a2865a6404762212
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-09 03:18:10 +0530

    welcome

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Length line = new Length(1, 2, 3, 4);
+            line.CalculateLength();
+            Console.ReadLine();
+        }
+    }
+}
